4648ccb692b39590091c14f6a864566ef07b8651
Vince <vincebel@protonmail.com>
2019-07-17 15:13:47 -0400

    blank file appending

--- client.go
+++ client.go
@@ -2,17 +2,32 @@ package main
 
 import( "fmt"
 	"encoding/json"
-//	"os"
-	"io/ioutil"
+//	"io/ioutil"
+	"os"
+	"log"
 )
 
 type network struct {
-	name []string
+	name []string `json:"name"`
+	author []string `json:"author"`
 }
 
 func main() {
-	fmt.Println("test")
+	fmt.Println("ltdnet v0.0.1")
+	var net []network
+	net = append(net, network{
+		name: []string{"Test Name"},
+		author: []string{"Vince"},
+	})
 
-	myint, _ := json.Marshal(3)
-	fmt.Println(string(myint))
+	// Print to demonstrate
+	marshString, _ := json.MarshalIndent(net, "", " ")
+	fmt.Println(string(marshString))
+
+	// Write to file
+	f, err := os.OpenFile("file.json", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.Write(marshString)
 }
