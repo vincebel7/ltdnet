539da2645085395cce41aa6ff42b7b84ba34bb07
vince <vincebel@protonmail.com>
2019-07-17 23:43:24 -0400

    0.0.3 working JSON write and idgen

--- client.go
+++ client.go
@@ -1,29 +1,85 @@
 package main
 
-import( "fmt"
+import(
+	"fmt"
 	"encoding/json"
-//	"io/ioutil"
 	"os"
 	"log"
+	"math/rand"
+	"time"
+	"bufio"
 )
 
-type network struct {
-	name []string `json:"name"`
-	author []string `json:"auth"`
-	gateway []string `json:"gway"`
+type Network struct {
+	ID	string
+	Name	string
+	Auth	string
+	Gway	string
 }
 
+var idchars = []rune("abcdef1234567890")
+
+func mainmenu() {
+	fmt.Println("ltdnet v0.0.3")
+
+	selection := false
+	for selection == false {
+	fmt.Println("Please create or select a network:")
+	fmt.Println("1) Create new network")
+	fmt.Println("2) Select saved network")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	option := scanner.Text()
+
+	if option == "1" || option == "C" || option == "c" || option == "create" || option == "new" {
+		selection = true
+		newnetwork()
+	} else if option == "2" || option == "S" || option == "s" || option == "select" {
+		selection = true
+		selectnetwork()
+	} else {
+		fmt.Println("Not a valid option.\n")
+		time.Sleep(500000000)
+	}
+}
+}
+
+func newnetwork() {
+	fmt.Println("Creating a new network")
+}
+
+func selectnetwork() {
+	fmt.Println("Please select a saved network")
+}
+
+func idgen() string {
+	id := make([]rune, 8)
+
+	rand.Seed(time.Now().UnixNano())
+	for i := range id {
+		id[i] = idchars[rand.Intn(len(idchars))]
+	}
+
+	return string(id)
+}
+
+
 func main() {
-	fmt.Println("ltdnet v0.0.2")
-	var net []network
-	net = append(net, network{
-		name: []string{"Test Name"},
-		author: []string{"Vince"},
-		gateway: []string{"192.168.1.1"},
+	mainmenu()
+
+	var net []Network
+	net = append(net, Network{
+		ID: idgen(),
+		Name: "Test Name",
+		Auth: "Vince",
+		Gway: "192.168.1.1",
 	})
 
 	// Print to demonstrate
-	marshString, _ := json.MarshalIndent(net, "", " ")
+	marshString, err := json.MarshalIndent(net, "", " ")
+	if err != nil {
+		log.Println(err)
+	}
 	fmt.Println(string(marshString))
 
 	// Write to file
@@ -32,4 +88,5 @@ func main() {
 		log.Fatal(err)
 	}
 	f.Write(marshString)
+	f.Write([]byte("\n"))
 }
