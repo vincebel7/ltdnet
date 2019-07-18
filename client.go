01b3155675b73a422bdea67b45e13319aee3192b
vincebel <vincebel@protonmail.com>
2019-07-18 00:17:58 -0400

    0.0.4 savefile names, savewipe script, network class

--- client.go
+++ client.go
@@ -8,6 +8,7 @@ import(
 	"math/rand"
 	"time"
 	"bufio"
+	"strings"
 )
 
 type Network struct {
@@ -15,12 +16,13 @@ type Network struct {
 	Name	string
 	Auth	string
 	Gway	string
+	Clas	string
 }
 
 var idchars = []rune("abcdef1234567890")
 
 func mainmenu() {
-	fmt.Println("ltdnet v0.0.3")
+	fmt.Println("ltdnet v0.0.4")
 
 	selection := false
 	for selection == false {
@@ -46,6 +48,57 @@ func mainmenu() {
 
 func newnetwork() {
 	fmt.Println("Creating a new network")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Your new network's name: ")
+	scanner.Scan()
+	network_name := scanner.Text()
+
+	fmt.Print("\nYour name: ")
+	scanner.Scan()
+	user_name := scanner.Text()
+
+	class_valid := false
+	network_class := "C"
+	for class_valid == false {
+		fmt.Print("\nNetwork class (A, B, or C): ")
+		scanner.Scan()
+		network_class = scanner.Text()
+		network_class = strings.ToUpper(network_class)
+
+		if network_class == "A" ||
+		network_class == "B" ||
+		network_class == "C" {
+			class_valid = true
+		}
+	}
+
+	var net []Network
+	net = append(net, Network{
+		ID: idgen(),
+		Name: network_name,
+		Auth: user_name,
+		Gway: "192.168.1.1",
+		Clas: network_class,
+	})
+
+	// Print to demonstrate
+	marshString, err := json.MarshalIndent(net, "", " ")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(string(marshString))
+
+	// Write to file
+	filename := "saves/" + network_name + ".json"
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.Write(marshString)
+	f.Write([]byte("\n"))
+
+	fmt.Println("\nNetwork created!")
 }
 
 func selectnetwork() {
@@ -66,27 +119,4 @@ func idgen() string {
 
 func main() {
 	mainmenu()
-
-	var net []Network
-	net = append(net, Network{
-		ID: idgen(),
-		Name: "Test Name",
-		Auth: "Vince",
-		Gway: "192.168.1.1",
-	})
-
-	// Print to demonstrate
-	marshString, err := json.MarshalIndent(net, "", " ")
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Println(string(marshString))
-
-	// Write to file
-	f, err := os.OpenFile("file.json", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
-	if err != nil {
-		log.Fatal(err)
-	}
-	f.Write(marshString)
-	f.Write([]byte("\n"))
 }
