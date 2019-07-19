8561e5e9630480e37a15867555ca86db134c03c6
vincebel <vincebel@protonmail.com>
2019-07-18 21:44:49 -0400

    v0.0.6 action menu, add/save routers

--- client.go
+++ client.go
@@ -15,16 +15,37 @@ import(
 )
 
 type Network struct {
-	ID	string `json:"id"`
-	Name	string `json:"name"`
-	Author	string `json:"auth"`
-	Gateway	string `json:"gway"`
-	Class	string `json:"clas"`
+	ID		string `json:"id"`
+	Name		string `json:"name"`
+	Author		string `json:"auth"`
+	Class		string `json:"clas"`
+	Router		Router `json:"rtr"`
 }
 
+var snet Network //selected network, essentially the loaded save file
+
+type Router struct {
+	ID		string `json:"id"`
+	Model		string `json:"modl"`
+	MACAddr		string `json:"maca"`
+	Hostname	string `json:"hnme"`
+	Gateway		string `json:"gway"`
+	DHCPPool	int `json:"dpol"` //maximum, not just available
+	Downports	int `json:"dpts"`
+}
+
+type Host struct {
+	ID		string `json:"id"`
+	Model		string `json:"modl"`
+	MACAddr		string `json:"maca"`
+	Hostname	string `json:"hnme"`
+	IPAddr		string `json:"ipa"`
+	SubnetMask	string `json:"mask"`
+	DefaultGateway	string `json:"gway"`
+}
 
 func mainmenu() {
-	fmt.Println("ltdnet v0.0.5")
+	fmt.Println("ltdnet v0.0.6")
 
 	selection := false
 		for selection == false {
@@ -75,15 +96,13 @@ func newnetwork() {
 		}
 	}
 
-	netid := idgen()
-	var net []Network
-	net = append(net, Network{
+	netid := idgen(8)
+	net := Network{
 		ID: netid,
 		Name: netname,
 		Author: user_name,
-		Gateway: "192.168.1.1",
 		Class: network_class,
-	})
+	}
 
 	// Print to demonstrate
 	marshString, err := json.Marshal(net)
@@ -94,7 +113,7 @@ func newnetwork() {
 
 	// Write to file
 	filename := "saves/" + netname + ".json"
-	f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0660)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -144,8 +163,6 @@ func selectnetwork() {
 }
 
 func loadnetwork(netname string) {
-	fmt.Printf("Selecting \"%s\"\n", netname)
-
 	//open file
 	filename := "saves/" + netname + ".json"
 	f, err := ioutil.ReadFile(filename)
@@ -153,17 +170,21 @@ func loadnetwork(netname string) {
 		fmt.Printf("File not found: %s", filename)
 	}
 
+	//unmarshal
 	var net Network
 	err2 := json.Unmarshal(f, &net)
 	if err2 != nil {
 		fmt.Printf("err: %v", err2)
 	}
-	fmt.Println(net)
+
+	//save global
+	snet = net
+	fmt.Println("Loaded %s", snet.Name)
 }
 
-func idgen() string {
+func idgen(n int) string {
 	var idchars = []rune("abcdef1234567890")
-	id := make([]rune, 8)
+	id := make([]rune, n)
 
 	rand.Seed(time.Now().UnixNano())
 	for i := range id {
@@ -173,6 +194,97 @@ func idgen() string {
 	return string(id)
 }
 
+func macgen() string {
+	mac := idgen(2)
+	for n := 0; n < 5; n++ {
+		mac = mac + ":" + idgen(2)
+	}
+	return mac
+}
+
+func NewBobcat(hostname string) Router {
+	b := Router{}
+	b.ID = idgen(8)
+	b.Model = "Bobcat 100"
+	b.MACAddr = macgen()
+	b.Hostname = hostname
+	b.DHCPPool = 253
+	b.Downports = 4
+
+	if snet.Class == "A" {
+		b.Gateway = "10.0.0.1"
+	} else if snet.Class == "B" {
+		b.Gateway = "172.16.0.1"
+	} else if snet.Class == "C" {
+		b.Gateway = "192.168.0.1"
+	}
+
+	fmt.Println(b)
+	return b
+}
+
+func addRouter() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("What model?")
+	fmt.Println("Available: Bobcat")
+	fmt.Print("Model: ")
+	scanner.Scan()
+	routerModel := scanner.Text()
+	routerModel = strings.ToUpper(routerModel)
+
+	fmt.Print("Hostname: ")
+	scanner.Scan()
+	routerHostname := scanner.Text()
+	r := Router{}
+	if routerModel == "BOBCAT" {
+		r = NewBobcat(routerHostname)
+	}
+	snet.Router = r
+}
+
+func actions() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("\nPlease type an action:")
+	fmt.Println("help")
+	fmt.Println("show network overview")
+	fmt.Println("add device router")
+	fmt.Println("del device router")
+	fmt.Print("> ")
+	scanner.Scan()
+	action_selection := scanner.Text()
+	if action_selection == "add device router" {
+		addRouter()
+		save()
+	}
+	if action_selection == "show network overview" {
+		fmt.Println(snet)
+	}
+}
+
+func save() {
+	//are you sure prompt
+
+	marshString, err := json.Marshal(snet)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println("Saving", string(marshString))
+	// Write to file
+	filename := "saves/" + snet.Name + ".json"
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0660)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.Write(marshString)
+	f.Write([]byte("\n"))
+
+	fmt.Println("Network saved")
+}
+
 func main() {
 	mainmenu()
+
+	for true {
+		actions()
+	}
 }
